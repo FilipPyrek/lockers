4049a8b96d646410ebaf40d934ee674b0712d005
Filip Pyrek <PyrekFilip@gmail.com>
2018-03-27 10:50:48 +0200

    Changed theme

--- app/theme.js
+++ app/theme.js
@@ -1,8 +1,27 @@
 import { createMuiTheme } from 'material-ui/styles';
-import blue from 'material-ui/colors/blue';
+import green from 'material-ui/colors/green';
+import deepOrange from 'material-ui/colors/deepOrange';
+import red from 'material-ui/colors/red';
 
 export default createMuiTheme({
   palette: {
-    primary: blue,
+    primary: {
+      light: green['200'],
+      main: green['600'],
+      dark: green['900'],
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: deepOrange['200'],
+      main: deepOrange['600'],
+      dark: deepOrange['900'],
+      contrastText: '#fff',
+    },
+    error: {
+      light: red['200'],
+      main: red['600'],
+      dark: red['900'],
+      contrastText: '#fff',
+    },
   },
 });
